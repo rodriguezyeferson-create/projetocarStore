a196122fd9d38712b995295d0699e6d093a292ea
rodriguezyeferson-create <rodriguez.yeferson@escola.pr.gov.br>
2026-05-04 11:06:28 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+function salvarCarros(event) {
+    event.preventdefault();
+
+    let titulo = document.getElementById('title').Value;
+    let preco = document.getElementById('preco').Value;
+    let marca = document.getElementById('marca').Value;
+    let modelo = document.getElementById('modelo').Value;
+}
+
